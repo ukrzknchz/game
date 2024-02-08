41a1cc59a342b2cc6d7ea005ecf47d0e9256caf4
ukrzknchz <sashapastushenkoschool@gmail.com>
2024-02-08 03:26:28 +0200

    6_4

---
+++ logs.js
@@ -0,0 +1,12 @@
+const logs = [
+    `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} поперхнулся, и за это ${secondPerson.name}с испугу приложил прямой удар коленом в лоб врага. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name} приняв волевое решение, неслышно подойдя сзади, ударил. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name}случайно нанес мощнейший удар. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name}нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} удивился, а ${secondPerson.name}пошатнувшись влепил подлый удар. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name}провел дробящий удар. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name}беспричинно ударил в ногу противника. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name}случайно влепил стопой в живот соперника. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+    `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. -${damage}, [${firstPerson.damageHP}/${firstPerson.defaultHP}]`
+];
