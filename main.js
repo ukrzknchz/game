9bfe3347746c0883d43e4dfc0e0a13297b900ae0
ukrzknchz <sashapastushenkoschool@gmail.com>
2024-02-08 02:36:32 +0200

    6_2

--- main.js
+++ main.js
@@ -8,6 +8,27 @@ const fighter = {
         damageHP: 100,
         elHP: document.getElementById("health-character"),
         elProgressbar: document.getElementById("progressbar-character"),
+        renderHP: function () {
+            this.renderHPLife();
+            this.renderProgressbarHP();
+        },
+        renderHPLife: function () {
+            this.elHP.innerText = this.damageHP + '/' + this.defaultHP;
+        },
+        renderProgressbarHP: function () {
+            this.elProgressbar.style.width = this.damageHP + '%';
+        },
+        changeHP: function (count) {
+            if (this.damageHP < count) {
+                this.damageHP = 0;
+                alert("Бедный " + this.name + " проиграл бой!");
+                $btnKick.disabled = true;
+                $btnSpecial.disabled = true;
+            } else {
+                this.damageHP -= count;
+            }
+            this.renderHP();
+        },
     },
     enemy: {
         name: 'Charmander',
@@ -15,56 +36,47 @@ const fighter = {
         damageHP: 100,
         elHP: document.getElementById("health-enemy"),
         elProgressbar: document.getElementById("progressbar-enemy"),
+        renderHP: function () {
+            this.renderHPLife();
+            this.renderProgressbarHP();
+        },
+        renderHPLife: function () {
+            this.elHP.innerText = this.damageHP + '/' + this.defaultHP;
+        },
+        renderProgressbarHP: function () {
+            this.elProgressbar.style.width = this.damageHP + '%';
+        },
+        changeHP: function (count) {
+            if (this.damageHP < count) {
+                this.damageHP = 0;
+                alert("Бедный " + this.name + " проиграл бой!");
+                $btnKick.disabled = true;
+                $btnSpecial.disabled = true;
+            } else {
+                this.damageHP -= count;
+            }
+            this.renderHP();
+        },
     },
 };
 
-$btnKick.addEventListener('click', function(){
+$btnKick.addEventListener('click', function () {
     console.log('Kick');
-    battleRound(20);
+    fighter.character.changeHP(20);
 });
 
-$btnSpecial.addEventListener('click', function(){
+$btnSpecial.addEventListener('click', function () {
     console.log('Special Attack');
-    battleRound(30); 
+    fighter.character.changeHP(30);
 });
 
 function init() {
     console.log('Start game!');
-    renderHP(fighter.character);
-    renderHP(fighter.enemy);
+    fighter.character.renderHP();
+    fighter.enemy.renderHP();
 }
 
-function renderHP(person){
-    renderHPLife(person);
-    renderProgressbarHP(person);
-}
-
-function renderHPLife(person){
-    person.elHP.innerText = person.damageHP + '/' + person.defaultHP;
-}
-
-function renderProgressbarHP(person){
-    person.elProgressbar.style.width = person.damageHP + '%';
-}
-
-function battleRound(damage){
-    changeHP(damage, fighter.character);
-    changeHP(random(20), fighter.enemy);
-}
-
-function changeHP(count, person){
-    if(person.damageHP < count) {
-        person.damageHP = 0;
-        alert("Бедный " + person.name + " проиграл бой!");
-        $btnKick.disabled = true;
-        $btnSpecial.disabled = true;
-    } else {
-        person.damageHP -= count;
-    }
-    renderHP(person);
-}
-
-function random(num){
+function random(num) {
     return Math.ceil(Math.random() * num);
 }
 
